ce9827485dadfece31b30bcbd9b7b77ec70e8ce6
lei.wei <lei.wei@phicomm.com>
2018-01-29 17:03:39 +0800

    spring

---
+++ spring-beans/src/main/java/org/springframework/beans/BeanDefinition.java
@@ -0,0 +1,74 @@
+package org.springframework.beans;
+
+import org.springframework.beans.factory.config.BeanMetadataElement;
+import org.springframework.core.AttributeAccessor;
+
+public interface BeanDefinition extends AttributeAccessor, BeanMetadataElement {
+
+	String SCOPE_SINGLETON = ConfigurableBeanFactory.SCOPE_SINGLETON;
+
+	String SCOPE_PROTOTYPE = ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
+
+	int ROLE_APPLICATION = 0;
+
+	int ROLE_SUPPORT = 1;
+
+	int ROLE_INFRASTRUCTURE = 2;
+
+
+	void setParentName(String parentName);
+
+	String getParentName();
+
+	void setBeanClassName(String beanClassName);
+
+	String getBeanClassName();
+
+	void setScope(String scope);
+	
+	String getScope();
+
+	void setLazyI
+	
+	boolean isLazyInit();
+
+
+	void setDependsOn(String... dependsOn);
+
+	String[] getDependsOn();
+
+	void setAutowireCandidate(boolean autowireCandidate);
+
+
+	void setPrimary(boolean primary);
+
+	boolean isPrimary();
+
+	void setFactoryBeanName(String factoryBeanName);
+
+	String getFactoryBeanName();
+
+	void setFactoryMethodName(String factoryMethodName);
+
+	String getFactoryMethodName();
+
+	ConstructorArgumentValues getConstructorArgumentValues();
+
+	MutablePropertyValues getPropertyValues();
+
+	boolean isSingleton();
+
+	boolean isPrototype();
+
+	boolean isAbstract();
+
+	int getRole();
+
+	String getDescription();
+
+	String getResourceDescription();
+
+	BeanDefinition getOriginatingBeanDefinition();
+
+}
